a08bbe61a5771ada5322f7d7440cef8fc4d70a30
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-22 01:37:15 +0900

    Create server_deluser.sh

---
+++ server_deluser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+userid=$1
+
+# deluser in gateway
+deluser --remove-home $userid
+
+# deluser in other nodes
+nodes="master node1 node2"
+for node in $nodes; do 
+ssh -T -A conmaster@$node << EOF
+bash
+deluser --remove-home \$userid
+EOF
+done
+ssh -T -A conmaster@storage << EOF
+bash
+deluser --remove-home \$userid
+rm -rf /data/connectome/\$userid
+EOF
