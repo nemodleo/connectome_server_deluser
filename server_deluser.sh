1f2cb33f82c5c5b0d631a15697811624c1614f71
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-22 19:22:20 +0900

    Update server_deluser.sh

--- server_deluser.sh
+++ server_deluser.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 userid=$1
+pw=$2
 
 # deluser in gateway
-deluser --remove-home $userid
+echo $pw | sudo -S deluser --remove-home $userid
 
 # deluser in other nodes
 nodes="master node1 node2"
 for node in $nodes; do 
-ssh -T -A conmaster@$node << EOF
+sshpass -p $pw ssh -T -A conmaster@$node << EOF
 bash
-deluser --remove-home $userid
+echo $pw | sudo -S deluser --remove-home $userid
 EOF
 done
-ssh -T -A conmaster@storage << EOF
+sshpass -p $pw ssh -T -A conmaster@storage << EOF
 bash
-deluser --remove-home $userid
-rm -rf /data/connectome/$userid
+echo $pw | sudo -S deluser --remove-home $userid
+echo $pw | sudo -S rm -rf /data/connectome/$userid
 EOF
